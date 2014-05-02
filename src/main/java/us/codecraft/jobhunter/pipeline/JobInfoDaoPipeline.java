a4b1ddc177629ce44198d2281cec324b4a2ebe2a
yihua.huang <code4crafter@gmail.com>
2014-05-02 17:29:45 +0800

    remove sysout

--- src/main/java/us/codecraft/jobhunter/pipeline/JobInfoDaoPipeline.java
+++ src/main/java/us/codecraft/jobhunter/pipeline/JobInfoDaoPipeline.java
@@ -21,7 +21,6 @@ public class JobInfoDaoPipeline implements PageModelPipeline<LieTouJobInfo> {
 
     @Override
     public void process(LieTouJobInfo lieTouJobInfo, Task task) {
-        System.out.println(lieTouJobInfo);
         jobInfoDAO.add(lieTouJobInfo);
     }
 }
